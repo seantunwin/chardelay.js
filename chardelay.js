02d0abd7b523490d4cb324afb22ab6ac70ac858d
Sean T. Unwin <sean.t.unwin@gmail.com>
2013-06-07 16:17:49 -0400

    Updated js file

--- chardelay.js
+++ chardelay.js
@@ -1,47 +1,84 @@
-var str = "Hello. I am creating a typing effect."
+/*
+ *     SETTINGS 
+ */
+/*Set String */
+var str = "You are a slave. Like everyone else, you were born into bondage, born into a prison that you cannot smell or taste or touch. A prison...for your mind....Unfortunatly, no one can be..._told_ what the Matrix is...you have to see it for yourself.";
 /* Set Array */
-var arr = [1,2,3,4,5,6,7,8];
-/* Create <p> element */
-var el = document.createElement('p');
-/* Write text inside <p> tag */
-function writeIt(dir, obj,index) {
-  /* If vertical then add a <br /> */
-  el.innerHTML += obj[index];
-  if (dir === 'v') {
-    el.innerHTML += '<br />';
-  }else{ return; }
-}
-/* Split string into character array */
-function splitStr(s){
-  s.split('');
-  return s;
-}
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+/* 
+ * Set object to iterate through
+ * In this case choose arr or str
+ */
+var obj = str;
+/* 
+ * Set direction
+ *  - 'h'= horizontal, 'v' = vertical
+ */
+var direction = 'h';
+/*  
+ * Set delay for outputing characters
+ *   - 0 is ignored and will use default
+ */
+var delay = 0;
+/*
+ * END SETTINGS *
+ */
+
+
 /* Initializer */
 function init() {
-  /* Set object to iterate through
-   * In this case choose arr or str
+  /* 
+   *      VARS
    */
-  var obj = str;
-  /* Set direction ('h'= horizontal, 'v' = vertical */
-  var direction = 'h';
-  /* Set Timeout Interval 
-   * Lower number for horizontal (faster)
+  /* 
+   * Type of element to create 
+   *   - String representation
    */
-  var time = (direction === 'v') ?  1000 : 150;
+  var el = 'p';
+  /* 
+   * Set Timeout Interval
+   *   - If delay is number and > 0 use it
+   *   - Defaults: 1s for vert; 150ms for horz
+   */
+  var dly = (typeof delay === 'number' && delay <= 0) ? ((direction === 'v') ?  1000 : 150) : delay;
   /* If obj is a string then convert to character array */
-  var newObj = (Array.isArray(obj)) ? obj : splitStr(obj);
+  var newObj = [];
+  /* Create element */
+  var elmt = document.createElement(el);
+  /***   END VARS   ***/
+  /*
+   *     FUNCTIONS
+   */
+  /* Split string into character array */
+  function splitStr(s){ 
+  s.split('');
+  return s;
+  }
+  /* Write text inside <p> tag */
+  function writeIt(ele, object, index, dir) {
+    /* If vertical then add a <br /> */
+    ele.innerHTML += object[index];
+    if (dir === 'v') {
+      ele.innerHTML += '<br />';
+    }else{ return; }
+  }
+  /***   END FUNCTIONS   ***/
+  /* Convert obj to character array if string */
+  newObj = (Array.isArray(obj)) ? obj : splitStr(obj);
+  /* Attach class to element */
+   elmt.className += 'chardelay';
   /* Attach <p> tag to document */
-  document.body.appendChild(el);
+  document.body.appendChild(elmt);
   /* Loop through Array */
   for (var i = 0; i < newObj.length; i++) {
   /* Anonymous IIFE passing vars i, newObj and time */
-    (function(x, o, t){
+    (function(e, o, x, d){
       /* Invoke delay */
       setTimeout(function(){
         /* Call write function */
-        writeIt('h',o,x);
-      },x * time); /* multiply to keep consistant interval on each loop*/
-    })(i, newObj, time);
+        writeIt(e, o, x, d);
+      },x * dly); /* multiply to keep consistant interval on each loop*/
+    })(elmt, newObj, i,  direction);
   }
 }
 /* Start */
