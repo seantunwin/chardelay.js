018a1e55c1c64da399025be1679aac07a6608423
seantunwin <sean.t.unwin@gmail.com>
2014-10-21 13:27:57 -0400

    Major Update: Added ability to add plain text to an element, added overwrite option to accomadate, added h1-h6 as valid tags

--- js/chardelay.js
+++ js/chardelay.js
@@ -1,6 +1,6 @@
 /**
 * chardelay.js - Delay the output of content in an HTML document
-*
+* @version : 1.0.1
 * @author: Sean T. Unwin
 * @contact: <sean DOT t DOT unwin AT gmail DOT com> <https://twitter.com/seantunwin>
 * @url: https://github.com/seantunwin/chardelay.js
@@ -9,41 +9,58 @@
 * var myVar = Chardelay(content, options);
 * @content: String or Array
 * @options:
-*       @layout     : String (h OR v)
-*       @delay      : Number (> 0)
-*       @inEl       : String (Elements accepted: p OR span OR div)
-*       @css        : String (CSS class name)
-*       @parentEl   : Object (HTML element)
-*       @multi      : Boolean 
+*       @parentEl : Object (HTML element)
+*       @delay : Number (> 0)
+*       @overwrite : Boolean
+*       @layout : String (h OR v)
+*       @inEl : String (Elements accepted: p OR span OR div)
+*       @css : String (CSS class name)
+*       @multi : Boolean
 *
-*************** 
-* Copyright 2013 Sean T. Unwin
+***************
+* Copyright 2013-2014 Sean T. Unwin
 * Released under the MIT license.
 * http://en.wikipedia.org/wiki/MIT_License
 */
-;(function () {
-	"use strict";
+(function (root, factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        // AMD environment
+        define('chardelay', [], function () {
+        return factory(root, document);
+    });
+    } else if (typeof exports === 'object') {
+        // CommonJS environment
+        module.exports = factory(root, document);
+    } else {
+        // Browser environment
+        root.Chardelay = factory(root, document);
+    }
+}(window, function () {
+    'use strict';
 
     function Chardelay(c, options) {
         var content,
             args = arguments,
             opts = {},
             defaults = {
-                "parentEl": window.document.body,
-                "layout": "h",
-                "delay": 150,
-                "inEl": "span",
-                "css": "chardelay",
-                "multi": false
+                'parentEl': window.document.body,
+                'delay': 150,
+                'overwrite': false,
+                'layout': 'h',
+                'inEl': 'span',
+                'css': 'chardelay',
+                'multi': false
             };
 
-        function contentConfig(){
+        function contentConfig() {
             // Remove extra index that IE8 adds to array
             function cleanContent(c) {
-                for (var z = 0; z <= c.length; z++) {
-                    if(c[z] == undefined) { 
+                var z;
+                for (z = 0; z <= c.length; z++) {
+                    if (c[z] === undefined) {
                         c.splice(z, 1);
-                    }                    
+                    }
                 }
                 return c;
             } /* END cleanContent */
@@ -54,62 +71,66 @@
 
             function validate(c) {
                 try {
-                    if (typeof c === "string" ) {
+                    if (typeof c === 'string') {
                         return splitStr(c);
                     } else if (Array.isArray(c)) {
                         return cleanContent(c);
-                    } else if (typeof c === "number") {
+                    } else if (typeof c === 'number') {
                         return splitStr(c.toString());
                     } else {
-                        throw new TypeError("content must be a String or Array");
+                        throw new TypeError('content must be a String or Array');
                     }
-                } catch(e) {
-                  window.console.log(e.name + ": " + e.message);
-                  content = false;
-                  return false;
+                } catch (e) {
+                    window.console.log(e.name + ': ' + e.message);
+                    content = false;
+                    return false;
                 }
             } /* END validate */
 
             try {
-                if(c === undefined){
-                    throw new Error("NoContent");
+                if (c === undefined || c === "") {
+                    throw new Error('NoContent');
                 } else {
                     content = validate(c);
                 }
-            } catch(e) {
-                window.console.log(e.name + ": " + e.message);
+            } catch (e) {
+                window.console.log(e.name + ': ' + e.message);
                 content = false;
                 return false;
             }
         } /* END contentConfig() */
-        function optionsConfig(){
+
+        function optionsConfig() {
             var optsLen,
                 isSet = true;
 
             function setup() {
 
-                var item,
-                    defsLen = Object.keys(defaults).length, /* Number of default options */
-                    argsLen = args.length; /* Number of initial arguments */
+                var defsLen = Object.keys(defaults).length, /* Number of default options */
+                    argsLen = args.length, /* Number of initial arguments */
+                    item;
                 /* Check for Options */
                 if (argsLen > 1) {
                     /* Options were passed as Object */
-                    if(argsLen === 2 && typeof args[1] === "object") {
+                    if (argsLen === 2 && typeof args[1] === 'object') {
+                        var  op,
+                            prop;
                         /* Get number of Options passed */
                         optsLen = Object.keys(options).length;
-                            item = 0;
-                            /* Check Properties for Options that match Defaults */
-                                for (var op in options) {
-                                    for ( var prop in defaults) {
-                                        if (op === prop) {
-                                            opts[prop] = options[prop];
-                                        } else if (typeof options[prop] === "undefined" ) {
-                                            opts[prop] = defaults[prop];
-                                        } else {
-                                            opts[op] = options[op];
-                                        }
+                        item = 0;
+
+                        /* Check Properties for Options that match Defaults */
+                            for (op in options) {
+                                for (prop in defaults) {
+                                    if (op === prop) {
+                                        opts[prop] = options[prop];
+                                    } else if (typeof options[prop] === 'undefined' ) {
+                                        opts[prop] = defaults[prop];
+                                    } else {
+                                        opts[op] = options[op];
                                     }
                                 }
+                            }
                     } else { /* Options passed as argument array */
                         optsLen = args.length-1;
                         for (item = 0; item <= defsLen-1; item++) {
@@ -139,58 +160,74 @@
                     }
                     return;
                 } /* END setAsOption */
-                
+
                 for (var i in defaults) {
                         tVal = opts[i];
                         switch (i) {
-                            case "parentEl":
-                                if (tVal && typeof tVal === "object") {
-                                    if (tVal instanceof window.jQuery || tVal.nodeType === 1 || Object.prototype.toString.call(tVal).match(/^\[object\s(.*)\]$/)[1] === "NodeList" || tVal.constructor.name === "NodeList") {
-                                        setAsOption(i, tVal[0]);
-                                    } else {
-                                        setAsDefault(i);
-                                    }
-                                } else {
-                                    setAsDefault(i);
-                                }
-                            break;
-                            case "layout":
-                                if (tVal && typeof tVal === "string") {
-                                    if (tVal=== "v" || tVal === "h") {
-                                        setAsOption(i);
-                                    } else {
-                                        setAsDefault(i);
-                                    }
-                                } else {
-                                    setAsDefault(i);
-                                }
-                            break;
-                            case "delay":
-                                if (tVal && typeof tVal === "number" && tVal > 0) {
-                                    setAsOption(i);
-                                } else {
-                                    setAsDefault(i);
-                                }
-                            break;
-                            case "inEl":
-                                if (/^\s*$/.test(tVal) !== false && typeof tVal === "string") {
+                            case 'parentEl':
+                                if (tVal && typeof tVal === 'object') {
 
-                                    if(tVal === "p" || tVal === "span" || tVal === "div") {
+                                    if (window.jQuery && tVal instanceof window.jQuery
+                                        || tVal.nodeType === 1
+                                        || Object.prototype.toString.call(tVal).match(/^\[object\s(.*)\]$/)[1] === 'NodeList'
+                                        || tVal.constructor.name === 'NodeList'
+                                        )
+                                    {
+                                        setAsOption(i, tVal[0]);
+                                    }
+                                    if (tVal instanceof HTMLElement) {
+                                        setAsOption(i);
+                                    }
+                                } else {
+                                    setAsDefault(i);
+                                }
+                            break;
+                            case 'delay':
+                                if (tVal && typeof tVal === 'number' && tVal > 0) {
+                                    setAsOption(i);
+                                } else {
+                                    setAsDefault(i);
+                                }
+                            break;
+                            case 'overwrite':
+                                if(tVal &&typeof tVal === 'boolean') {
+                                    if(tVal === true) {
+                                        setAsOption(i);
+                                    } else {
+                                        setAsDefault(i);
+                                    }
+                                }
+                                break;
+                            case 'layout':
+                                if (tVal && typeof tVal === 'string') {
+                                    if (tVal=== 'v' || tVal === 'h') {
+                                        setAsOption(i);
+                                    } else {
+                                        setAsDefault(i);
+                                    }
+                                } else {
+                                    setAsDefault(i);
+                                }
+                            break;
+                            case 'inEl':
+                                if (/^\s*$/.test(tVal) !== false && typeof tVal === 'string') {
+
+                                    if(tVal === 'p' || tVal === 'span' || tVal === 'div' || tVal === /[Hh][1-6]/.test(tVal) || tVal === 'ul') {
                                         setAsOption(i);
                                     } else {
                                         setAsDefault(i);
                                     }
                                 }
                             break;
-                            case "css":
-                                if (tVal && typeof tVal === "string") {
+                            case 'css':
+                                if (tVal && typeof tVal === 'string') {
                                     setAsOption(i);
                                 } else {
                                     setAsDefault(i);
                                 }
                             break;
-                            case "multi":
-                                if(tVal && typeof tVal === "boolean" && tVal === true) {
+                            case 'multi':
+                                if(tVal && typeof tVal === 'boolean' && tVal === true) {
                                     setAsOption(i);
                                 } else {
                                     setAsDefault(i);
@@ -215,24 +252,22 @@
             function createElmt(v) {
                 v = document.createElement(opts.inEl);
                     v.className += opts.css;
-                if (opts.multi && opts.layout === "v") {
-                    v.style.display = "table";
-                    v.style.verticalAlign = "middle";
+                if (opts.multi && opts.layout === 'v') {
+                    v.style.display = 'table';
+                    v.style.verticalAlign = 'middle';
                 }
                 return v;
             } /* END createElmt */
 
             function contentToElmt(el, item, dir) {
                 el.innerHTML += content[item];
-                if (dir === "v") {
+                if (dir === 'v') {
                     if (!opts.multi) {
-                      el.innerHTML += "<br />";
-                    } else {
-                        return;
+                      el.innerHTML += '<br />';
                     }
                 }
             } /* END contentToElmt() */
-            
+
             function doDelay(el, x, dir, dly) {
                 setTimeout(function(){
                     if (opts.multi) {
@@ -247,16 +282,22 @@
                     elArr = [];
 
                 if (!opts.multi) {
-                    el = createElmt(el);
-                    writeIt(el);
+                    if(!opts.overwrite) {
+                        el = createElmt(el);
+                        writeIt(el);
+                    }
                 }
                 for (var i = 0; i < content.length; i++) {
-                    if(!opts.multi) {
-                        doDelay(el, i, opts.layout, opts.delay);
+                    if (opts.overwrite) {
+                        doDelay(opts.parentEl, i, opts.layout, opts.delay);
                     } else {
-                        el = createElmt(el);
-                        elArr.push(el);
-                        doDelay(elArr[i], i, opts.layout, opts.delay);
+                        if (!opts.multi) {
+                                doDelay(el, i, opts.layout, opts.delay);
+                        } else {
+                            el = createElmt(el);
+                            elArr.push(el);
+                            doDelay(elArr[i], i, opts.layout, opts.delay);
+                        }
                     }
                 }
            } /* END setStage */
@@ -270,7 +311,7 @@
 
         function init() {
             contentConfig();
-            if (content === false){return false;}
+            if (content === false){ return false; }
             optionsConfig();
             writeToDOM();
         } /* END init */
@@ -290,7 +331,7 @@
          */
         if(!Array.isArray) {
           Array.isArray = function (vArg) {
-            return Object.prototype.toString.call(vArg) === "[object Array]";
+            return Object.prototype.toString.call(vArg) === '[object Array]';
           };
         }
     })();
@@ -302,7 +343,7 @@
          */
         if (!Object.keys) Object.keys = function(o) {
             if (o !== Object(o)) {
-            throw new TypeError("Object.keys called on a non-object");
+            throw new TypeError('Object.keys called on a non-object');
             }
             var k=[],p;
             for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
@@ -312,21 +353,6 @@
     })();
     /* END Dependencies */
 
-   /**
-    * Expose Chardelay
-    */
-    // AMD suppport
-    if (typeof window.define === "function" && window.define.amd !== undefined) {
-        window.define("Chardelay", [], function () {
-            return Chardelay;
-            });
+   return Chardelay;
 
-    // CommonJS suppport
-    } else if (typeof module !== "undefined" && module.exports !== undefined) {
-        module.exports = Chardelay;
-
-    // Default
-    } else {
-        window.Chardelay = Chardelay;
-    }
-}());
+}));
